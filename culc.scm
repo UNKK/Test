cccdef0f270184696de91dd11e443b122ac8ba29
anakuro@gmail.com <unkk@UNKK-MAC.local>
2011-01-27 22:51:36 +0900

    Second commit

--- culc.scm
+++ culc.scm
@@ -24,4 +24,19 @@
      (else (cons (car pile) (shuffle (cdr pile))))))
   (cond
    ((null? pile) '())
-   (re-shuffle (swap-top pile (random-integer (length pile))))))
+   (else (re-shuffle (swap-top pile (random-integer (length pile)))))))
+
+(define trunp-pile
+  (fold
+   (lambda (mark prevNums)
+     (append
+      (fold 
+       (lambda (num prev)
+	 (cons (cons mark num) prev))
+       '()
+       '(1 2 3 4 5 6 7 8 9 10 11 12 13))
+      prevNums))
+   '()
+   '(:H :D :S :C)))
+
+(shuffle trunp-pile)
